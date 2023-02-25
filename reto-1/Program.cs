91c7dcdec01c0b7010860ab69063efe468847c82
D3MB3 <110141509+YostinMejia@users.noreply.github.com>
2023-02-24 22:13:53 -0500

    Final

--- reto-1/Program.cs
+++ reto-1/Program.cs
@@ -7,9 +7,19 @@ using System.Threading.Tasks;
 {
         static void Main(string[] args)
     {
+        Console.WriteLine("Bienvenid@ \n ¿Cual es su id?");
+        string id_invitado = Console.ReadLine();
+        Console.WriteLine("¿Cual es la ruta del archivo?");
+        string file_path = Console.ReadLine();
+        
+        if (file_path != "Taller_herencia.txt" && file_path != "Taller_herencia.csv"){
+            Console.WriteLine("Ese archivo no existe ");
+        }
+        else{
+            Taquilla guardia = new Taquilla();
+            Console.WriteLine(guardia.verificar_txt_csv(file_path, id_invitado));
+        }
 
-        Taquilla guardia = new Taquilla();
-        Console.WriteLine(guardia.verificar_txt_csv("Taller_herencia.txt","6573512"));
         
         
     }
