fc076342a53e297900c406f695d1964b52ea7a78
D3MB3 <110141509+YostinMejia@users.noreply.github.com>
2023-04-07 23:02:08 -0500

    rotten tomateos

--- webscraping/Program.cs
+++ webscraping/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 
 namespace webscraping
@@ -8,11 +9,19 @@ namespace webscraping
         static void Main(string[] args)
         {
 
-            //Console.WriteLine( web_scraper.call_url("https://finance.yahoo.com/crypto/").Result);
-            //Console.WriteLine( web_scraper.get_books_data("https://www.goodreads.com/choiceawards/best-science-fiction-books-2022"));
-            List<List<string>> cryptos = web_scraper.get_books_data("https://finance.yahoo.com/crypto/");
+            //pelicula.DatosPelicula("https://www.rottentomatoes.com/m/everything_everywhere_all_at_once");
+            //pelicula.DatosPelicula("https://www.rottentomatoes.com/m/praise_this");
+            //pelicula.DatosPelicula("https://www.rottentomatoes.com/serie/the_night_agent/s01");
+            
+            //pelicula.DatosPelicula("https://www.rottentomatoes.com/m/little_richard_i_am_everything");
+            //serie.DatosPelicula("https://www.rottentomatoes.com/m/air_2023");
+            //serie.DatosPelicula("https://www.rottentomatoes.com/m/leave_no_trace");
 
-         
+            //serie.DatosPelicula("https://www.rottentomatoes.com/serie/the_night_agent");
+            //serie.DatosPelicula("https://www.rottentomatoes.com/serie/the_big_door_prize");
+            //serie.DatosPelicula("https://www.rottentomatoes.com/tv/the_big_door_prize/s01");
+            serie.DatosPelicula("https://www.rottentomatoes.com/tv/the_mandalorian/s03");
+
         }
     }
 }
---
+++ webscraping/seriescraper.cs
@@ -0,0 +1,121 @@
+﻿using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+namespace webscraping
+{
+    internal class serie:pelicula
+    {
+        //Envia y recibe una respuesta a la direccion por medio de la url
+        public static async Task<string> call_url(string url)
+        {
+
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+
+            //Retorna todo el html
+            return response;
+        }
+
+
+        private static List<List<string>> ActoresPrincipales(string html)
+        {
+            return pelicula.ActoresPrincipales(html);
+        }
+
+        //los comentarios estan en las sesiones y no en la pagina principal
+        //Hay que mirar si el link contiene s## que indica el numero de la serie para saber si se pueden mirar los comentarios o no
+        private static List<List<string>> ComentariosCriticos(string url)
+        {
+
+            if (!url.Contains("/s"))
+            {
+                Console.WriteLine(  "Los comentarios están en las paginas de las temporadas, no en la principal");
+                return new List<List<string>>();
+            }
+
+
+            return pelicula.ComentariosCriticos(url);
+
+
+        }
+        private static List<List<string>> ComentariosUsuarios(string url)
+        {
+            
+            if(! url.Contains("/s"))
+            {
+                Console.WriteLine("Los comentarios están en las paginas de las temporadas, no en la principal");
+
+                return new List<List<string>>();
+            }
+            
+            return pelicula.ComentariosUsuarios(url, false);
+        }
+
+
+        private static List<List<string>> PlataformasDisponibles(string html)
+        {
+            return pelicula.PlataformasDisponibles(html);
+
+        }
+
+
+        private static List<List<string>> Calificacion(string url, string html)
+        {
+
+            if (!url.Contains("/s"))
+            {
+                Console.WriteLine("los contadores están en las paginas de las temporadas, no en la principal");
+
+                return pelicula.Calificacion(html, false);
+            }
+
+            return pelicula.Calificacion(html, false);
+
+        }
+
+        private static List<List<string>> DatosPrincipales(string url, string html )
+        {
+            
+            if (! url.Contains("/s"))
+            {
+
+                return pelicula.DatosPrincipales(html, false, false);
+            }
+
+            return pelicula.DatosPrincipales(html, false, true);
+
+        }
+        public static List<string> DatosPelicula(string url)
+        {
+            string response = call_url(url).Result;
+            //serie
+            List<List<string>> comentarios_criticos = ComentariosCriticos(url);
+            List<List<string>> comentarios_verificados = ComentariosUsuarios(url);
+            List<List<string>> plataformas = PlataformasDisponibles(response);
+            List<List<string>> actores = ActoresPrincipales(response);
+            List<List<string>> calificacion = Calificacion(url,response);
+            List<List<string>> datos_princi = DatosPrincipales(url, response);
+
+
+            List<string> datos = new List<string>();
+
+            return datos;
+        }
+
+
+
+
+    }
+
+
+}
+
